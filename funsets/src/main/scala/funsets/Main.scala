2ec1e4c769dcc94d9919c13e98659a6365be4d32
Olu Oyedipe <ooyedipe@gmail.com>
2015-08-24 22:30:13 -0700

    added one more printset command

--- funsets/src/main/scala/funsets/Main.scala
+++ funsets/src/main/scala/funsets/Main.scala
@@ -1,11 +1,13 @@
 package funsets
-
+import math.abs
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
-  def s: Set = (x: Int) => x % 12 == 0
+  def s: Set = (x: Int) => x % 500 == 0
   def p: Int => Boolean = (x:Int) => x % 5 == 0
   println(forall(s,p))
   println(exists(s,p))
   println(printSet(s))
+
+  println(printSet(map(s,x => x*2 )))
 }
