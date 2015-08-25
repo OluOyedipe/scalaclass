82e1f819f75e6489742dcbc005ea18f0e7ee6d9c
Olu Oyedipe <oo185005@teradata.com>
2015-08-24 18:15:39 -0700

    coded forall and exists

--- funsets/src/main/scala/funsets/Main.scala
+++ funsets/src/main/scala/funsets/Main.scala
@@ -3,4 +3,8 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+  def s: Set = (x: Int) => x % 12 == 0
+  def p: Int => Boolean = (x:Int) => x % 5 == 0
+  println(forall(s,p))
+  println(exists(s,p))
 }
